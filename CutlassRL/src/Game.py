c820456a6fedca938be4c7e82e5f367d0171ac90
init <init@6158a3de-9821-11e0-a3a7-075e1a996d51>
2011-06-16 14:36:53 +0000

    main import
    git-svn-id: http://89.31.112.4/repos/CutlassRL_SVN@6 6158a3de-9821-11e0-a3a7-075e1a996d51

---
+++ CutlassRL/src/Game.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+#     This file is part of CutlassRL.
+#
+#    CutlassRL is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    CutlassRL is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with CutlassRL.  If not, see <http://www.gnu.org/licenses/>.
+
+VERSION = 0.01;
+
+try:
+    import libtcodpy as libtcod # Loading libtcod library
+except ImportError:
+    print "Libtcod is missing."
+    exit()
+try:
+    import curses               # Game will use curses to draw things
+    screen = curses;
+except ImportError:
+    print "Curses library is missing."
+    exit()
+class Game:                # Main game class
+    def __init__(self):
+        """Initializer of Game class.
+            Will start curses.
+        """
+        screen.initscr()
+        screen.curs_set(0)
+        screen.cbreak()
+        screen.start_color()
+        screen.use_default_colors()
+
+    def mainloop(self):
+        """Mainloop of game.
+            Useless for now.
+        """
+    def end(self):
+        """End of game.
+            Will reset console.
+        """
+        screen.endwin()
+        
+    def debug_message(self):
+        """Debug message subroutine,
+            Will say something like DEBUG: XXX
+        """
---
+++ CutlassRL/src/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#     This file is part of CutlassRL.
+#
+#    CutlassRL is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    CutlassRL is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with CutlassRL.  If not, see <http://www.gnu.org/licenses/>.
+
+# CutlassRL - roguelike game.
+
+VERSION = 0.01;
+
+import Game  # importing main game library
+
+play = Game.Game()
+play.mainloop()
+play.end()
+del play
